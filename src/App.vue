f548d80c7d0a8fc12db0957c3fefb0ee6108081e
Thet Htoo Htoo San <thethtoohtoosan2172000@gmail.com>
2024-04-27 23:51:20 +0630

    shortand binding

--- src/App.vue
+++ src/App.vue
@@ -7,19 +7,19 @@
       <div v-text="job"></div>
       <div v-html="channel"></div>
       <div v-html="hack"></div>
-      <h3 v-bind:id="headingId">Attributes Binding</h3>
-      <button v-bind:disabled="isDisabled">Bind</button>
+      <h3 :id="headingId">Attributes Binding</h3>
+      <button :disabled="isDisabled">Bind</button>
       <h2 class="underline">Underlined text</h2>
-      <h2 class="underline" v-bind:class="status">Status</h2>
-      <h2 v-bind:class="isPromoted && 'promoted'">Promoted movie</h2>
-      <h2 v-bind:class="isSolodout ? 'sold-out ' : 'new'">Soldout movies</h2>
-      <p v-bind:class="['new', 'promoted']">newly promoted movies</p>
-      <h3 v-bind:class="[isPromoted && 'promoted', isSolodout ? 'sold-out' : 'new']">
+      <h2 class="underline" :class="status">Status</h2>
+      <h2 :class="isPromoted && 'promoted'">Promoted movie</h2>
+      <h2 :class="isSolodout ? 'sold-out ' : 'new'">Soldout movies</h2>
+      <p :class="['new', 'promoted']">newly promoted movies</p>
+      <h3 :class="[isPromoted && 'promoted', isSolodout ? 'sold-out' : 'new']">
         Array approach binding class
       </h3>
 
       <h2
-        v-bind:class="{
+        :class="{
           promoted: isPromoted,
           new: !isSolodout,
           'sold-out': isSolodout
@@ -27,6 +27,16 @@
       >
         Object Conditional movie
       </h2>
+
+      <h2 :style="{ color: highlightColor, 'font-size': headerSize + 'px', padding: '20px' }">
+        Inline style binding
+      </h2>
+
+      <p :style="headerSytleObject">Systle object</p>
+
+      <div :style="[baseStyleObject, successStyleObject]">Success sytle</div>
+
+      <div :style="[baseStyleObject, dangerStyleObject]">danger style</div>
     </div>
   </header>
 
@@ -47,7 +57,29 @@ export default {
       isDisabled: true,
       status: 'success',
       isPromoted: true,
-      isSolodout: false
+      isSolodout: false,
+      highlightColor: 'orange',
+      headerSize: 50,
+      headerSytleObject: {
+        color: 'orange',
+        fontSize: '50px',
+        padding: '20px'
+      },
+      baseStyleObject: {
+        fontSize: '50px',
+        padding: '20px'
+      },
+      successStyleObject: {
+        color: 'green',
+        backgroundColor: 'lightgreen',
+        border: '1px solid black',
+        padding: '20px'
+      },
+      dangerStyleObject: {
+        color: 'darkred',
+        backgroundColor: 'red',
+        border: '1px solid darkred'
+      }
     }
   }
 }
