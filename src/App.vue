d0de9a05fcdb96103d246562d260a4338193b612
Thet Htoo Htoo San <thethtoohtoosan2172000@gmail.com>
2024-04-28 15:54:34 +0630

    v-if-elseif

--- src/App.vue
+++ src/App.vue
@@ -1,46 +1,19 @@
 <script setup></script>
 
 <template>
-  <header>
-    <div class="wrapper">
-      <div>{{ greet }} {{ name }}</div>
-      <div v-text="job"></div>
-      <div v-html="channel"></div>
-      <div v-html="hack"></div>
-      <h3 :id="headingId">Attributes Binding</h3>
-      <button :disabled="isDisabled">Bind</button>
-      <h2 class="underline">Underlined text</h2>
-      <h2 class="underline" :class="status">Status</h2>
-      <h2 :class="isPromoted && 'promoted'">Promoted movie</h2>
-      <h2 :class="isSolodout ? 'sold-out ' : 'new'">Soldout movies</h2>
-      <p :class="['new', 'promoted']">newly promoted movies</p>
-      <h3 :class="[isPromoted && 'promoted', isSolodout ? 'sold-out' : 'new']">
-        Array approach binding class
-      </h3>
+  <h2 v-if="num === 0">the num is zero</h2>
+  <h3 v-else-if="num < 0">the num is negative</h3>
+  <h3 v-else-if="num > 0">the num is positive</h3>
+  <h2 v-else>not a number</h2>
 
-      <h2
-        :class="{
-          promoted: isPromoted,
-          new: !isSolodout,
-          'sold-out': isSolodout
-        }"
-      >
-        Object Conditional movie
-      </h2>
+  <template v-if="display">
+    <h2>esther</h2>
+    <h2>codevolution</h2>
+    <h2>vue</h2>
+  </template>
 
-      <h2 :style="{ color: highlightColor, 'font-size': headerSize + 'px', padding: '20px' }">
-        Inline style binding
-      </h2>
-
-      <p :style="headerSytleObject">Systle object</p>
-
-      <div :style="[baseStyleObject, successStyleObject]">Success sytle</div>
-
-      <div :style="[baseStyleObject, dangerStyleObject]">danger style</div>
-    </div>
-  </header>
-
-  <RouterView />
+  <h2 v-show="showElement">using v-show</h2>
+  <h2 v-if="showElement">using v-if</h2>
 </template>
 
 <script>
@@ -48,38 +21,9 @@ export default {
   name: 'App',
   setup() {
     return {
-      greet: 'Hello',
-      name: 'Esther',
-      job: 'developer',
-      channel: '<h2> Code volution </h2>',
-      hack: `<a href="#" onclick="alert('you have been hacked')">win price</a>`,
-      headingId: 'headingId',
-      isDisabled: true,
-      status: 'success',
-      isPromoted: true,
-      isSolodout: false,
-      highlightColor: 'orange',
-      headerSize: 50,
-      headerSytleObject: {
-        color: 'orange',
-        fontSize: '50px',
-        padding: '20px'
-      },
-      baseStyleObject: {
-        fontSize: '50px',
-        padding: '20px'
-      },
-      successStyleObject: {
-        color: 'green',
-        backgroundColor: 'lightgreen',
-        border: '1px solid black',
-        padding: '20px'
-      },
-      dangerStyleObject: {
-        color: 'darkred',
-        backgroundColor: 'red',
-        border: '1px solid darkred'
-      }
+      num: 1,
+      display: true,
+      showElement: false
     }
   }
 }
